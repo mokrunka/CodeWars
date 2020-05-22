19ee55084970f9cd9ace51fea63ff96b4c59c34d
mokrunka <hunker.engineer@gmail.com>
2020-05-22 15:16:15 -0700

    added file greetme.py and ATM.py (initial commit only)

---
+++ ATM.py
@@ -0,0 +1,8 @@
+def solve(n):
+    if n % 10 != 0:
+        return -1
+
+
+solve(770)
+solve(1250)
+solve(125)
---
+++ greetme.py
@@ -0,0 +1,4 @@
+def greet(name):
+    return 'Hello ' + name.title() + '!'
+
+greet('riley')
