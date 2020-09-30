2401630b916b29268888c3f6b15e4f202205e1c3
mokrunka <hunker.engineer@gmail.com>
2020-09-30 10:11:42 -0700

    add matrixaddition.py

---
+++ GCDsum.py
@@ -0,0 +1,11 @@
+# find and return 2 numbers in ascending order based on their sums and greatest
+# common denominator (GCD)
+def solve(s,g):
+    pass
+
+
+solve(6,3)
+solve(8,2)
+solve(10,3)
+solve(12,4)
+solve(12,5)
---
+++ matrixaddition.py
@@ -0,0 +1,20 @@
+def matrix_addition(m1, m2):
+    sum_matrix = []
+    add_rows = zip(m1, m2)
+    for x, y in add_rows:
+        size = len(x)
+        for i in range(len(x)):
+            placeholder = x[i] + y[i]
+            i += 1
+            sum_matrix.append(placeholder)
+
+    result = m1
+    row = []
+    k = 0
+
+    for i in range(size):
+        for j in range(size):
+            result[i][j] = sum_matrix[k]
+            k += 1
+
+    return result
