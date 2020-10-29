32e5f9c395615e32b7bd05c550e680c0439d936c
mokrunka <hunker.engineer@gmail.com>
2020-10-29 14:19:41 -0700

    added arraysingles.py

---
+++ arraysingles.py
@@ -0,0 +1,18 @@
+def repeats(arr):
+    dup_list = []
+    sum = 0
+    for num in arr:
+        if arr.count(num) == 1 and (num not in dup_list):
+            dup_list.append(num)
+        else:
+            pass
+
+    return (dup_list[0] + dup_list[1])
+
+
+# another method, using list comprehensions
+# def repeats(arr):
+#     return sum([x for x in arr if arr.count(x) == 1])
+
+repeats([4,5,7,5,4,8])
+# repeats([9, 10, 19, 13, 19, 13])
